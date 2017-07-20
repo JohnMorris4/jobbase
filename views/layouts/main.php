0c9f29d808338042b8bc461cf2eb426adcbd750a
JohnMorris4 <johnmorris@morrisje.com>
2017-07-20 16:08:04 -0400

    Added initial tree structure and source files

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -74,7 +74,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Morris Web Designs | <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
